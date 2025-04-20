b29af967f7299ddcded66ada3ba78272dee63979
Paulo Sergio <contato.paulofws@gmail.com>
2025-04-20 17:06:37 -0300

    style: add animation and z-index changed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import { MenuMobile } from "./menu-mobile";
 
 export function Header() {
   return (
-    <header className="flex sticky top-4 items-center justify-end sm:justify-center transition-all ease-in-out duration-300">
+    <header className="flex sticky top-4 items-center justify-end sm:justify-center transition-all ease-in-out duration-300 animate-fade-down animate-once animate-duration-700 animate-delay-300 animate-ease-in z-50">
       <nav className="flex rounded-2xl border-0 sm:border border-ring/30 bg-secondary p-1 sm:shadow-sm">
         <div className="flex sm:hidden pr-4">
           <MenuMobile />
