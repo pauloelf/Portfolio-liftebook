1befdc64319c3a4ea556460ae20da308a8655e51
Paulo Sergio <contato.paulofws@gmail.com>
2025-04-29 09:01:13 -0300

    style: add min height in main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main className="container flex flex-col mx-auto p-4 sm:py-6 md:p-8">
+          <main className="container min-h-screen flex flex-col mx-auto p-4 sm:py-6 md:p-8">
             <div className="fixed right-8 bottom-4 animate-bounce animate-infinite animate-duration-1000 animate-delay-300 animate-ease-in z-10">
               <TooltipItem trigger="Role para baixo">
                 <Button
