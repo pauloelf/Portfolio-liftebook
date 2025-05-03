9469d7b894c719270914a4931dad815b6eaad07d
Paulo Sergio <contato.paulofws@gmail.com>
2025-05-03 20:47:37 -0300

    refactor: changed card-project layout

--- src/components/home/card-project.tsx
+++ src/components/home/card-project.tsx
@@ -20,31 +20,16 @@ export function CardProject({
 }: CardProjectInterface) {
   return (
     <article className="flex-1 animate-fade animate-once animate-duration-700 animate-delay-300 animate-ease-in">
-      <Card className="gap-2 pt-0">
+      <Card className="gap-2 pt-0 h-full">
         <div className="relative h-48 w-full rounded-t-xl bg-neutral-100 text-muted-foreground/30 flex justify-center items-center">
-          {/* <Image
+          <Image
             src={image.src}
             alt={image.alt}
             fill
             className="object-cover"
-          /> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="48"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-            <circle cx="9" cy="9" r="2" />
-            <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21" />
-          </svg>
+          />
         </div>
-        <div className="flex flex-col justify-between pt-4">
+        <div className="flex flex-col justify-between h-2/5 pt-4">
           <CardHeader className="font-secondary">
             <CardTitle className="uppercase">{title}</CardTitle>
             <CardDescription>{description}</CardDescription>
@@ -55,12 +40,12 @@ export function CardProject({
               variant="secondary"
               className="rounded-xl font-secondary font-bold text-card-foreground hover:text-secondary-foreground border border-foreground/50 bg-card hover:bg-secondary"
             >
-              <Link href={url}>
+              <Link href={url} target="_blank">
                 Ver Projeto <ExternalLink />
               </Link>
             </Button>
             <Button asChild variant="default" className="rounded-xl">
-              <Link href={codeURL}>
+              <Link href={codeURL} target="_blank">
                 Github <Github />
               </Link>
             </Button>
