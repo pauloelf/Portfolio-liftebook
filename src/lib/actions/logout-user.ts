e4cbb5df1f241f4a3a056fc7202e7901b42acf08
Paulo Sergio <contato.paulofws@gmail.com>
2025-05-04 13:33:10 -0300

    refactor: changed cookie key

--- src/lib/actions/logout-user.ts
+++ src/lib/actions/logout-user.ts
@@ -4,6 +4,6 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function logoutUser() {
-  (await cookies()).set("auth-token", "", { expires: new Date(0) });
+  (await cookies()).set("session", "", { expires: new Date(0) });
   redirect("/login");
 }
