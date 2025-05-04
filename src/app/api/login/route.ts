5f346e9aabff2ea8bbc9eefd87f07817d93473e6
Paulo Sergio <contato.paulofws@gmail.com>
2025-05-04 13:24:29 -0300

    feat: add login in api route

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,44 @@
+import { auth } from "firebase-admin";
+import { cookies, headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const authorization = (await headers()).get("Authorization");
+  if (authorization?.startsWith("Bearer ")) {
+    const idToken = authorization.split("Bearer ")[1];
+    const decodedToken = await auth().verifyIdToken(idToken);
+
+    if (decodedToken) {
+      const expiresIn = 60 * 60 * 24 * 5 * 1000;
+      const sessionCookie = await auth().createSessionCookie(idToken, {
+        expiresIn,
+      });
+      const options = {
+        name: "session",
+        value: sessionCookie,
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: true,
+      };
+
+      (await cookies()).set(options);
+    }
+  }
+
+  return NextResponse.json({}, { status: 200 });
+}
+export async function GET(req: NextRequest) {
+  const session = (await cookies()).get("session")?.value || "";
+
+  if (!session) {
+    return NextResponse.json({ isLogged: false }, { status: 401 });
+  }
+
+  const decodedClaims = await auth().verifySessionCookie(session, true);
+
+  if (!decodedClaims) {
+    return NextResponse.json({ isLogged: false }, { status: 401 });
+  }
+
+  return NextResponse.json({ isLogged: true }, { status: 200 });
+}
